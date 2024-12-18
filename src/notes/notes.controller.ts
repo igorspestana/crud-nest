4b29729ab900d7c82dfc8ca219e81399b93520d8
igorspestana <igorspestana@gmail.com>
2024-12-18 16:01:42 -0300

    Rota para atualizar um recado (update) com métodos PATCH ou PUT e decoradores

--- src/notes/notes.controller.ts
+++ src/notes/notes.controller.ts
@@ -5,6 +5,7 @@ import {
   HttpCode,
   HttpStatus,
   Param,
+  Patch,
   Post,
 } from '@nestjs/common';
 
@@ -32,5 +33,13 @@ export class NotesController {
   @Post()
   create(@Body() body: any) {
     return body;
+  }
+
+  @Patch('noteupdate/:id')
+  update(@Param('id') id: string, @Body() body: any) {
+    return {
+      id,
+      ...body,
+    };
   }
 }
