f9a7d7dd4bc73479a4e5593978240140bf9ba8b7
igorspestana <igorspestana@gmail.com>
2024-12-23 10:24:04 -0300

    Validando dados de entrada com useGlobalPipes, ValidationPipe e class-validator

--- src/notes/dto/create-note.dto.ts
+++ src/notes/dto/create-note.dto.ts
@@ -1,5 +1,19 @@
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+
 export class CreateNoteDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
+  @MaxLength(50)
   readonly text: string;
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
+  @MaxLength(50)
   readonly from: string;
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
+  @MaxLength(50)
   readonly to: string;
 }
