62a01fa62f9aa67d1db29e4252e7daa6481c3a56
igorspestana <igorspestana@gmail.com>
2025-01-16 16:06:28 -0300

    Primeira Entity do TypeOrm no NestJS (primeira tabela na base de dados PSQL)

--- src/notes/entities/note.entity.ts
+++ src/notes/entities/note.entity.ts
@@ -1,8 +1,31 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
 export class Note {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({ type: 'varchar', length: 255 })
   text: string;
+
+  @Column({ type: 'varchar', length: 50 })
   from: string;
+
+  @Column({ type: 'varchar', length: 50 })
   to: string;
+
+  @Column({ default: false })
   read: boolean;
-  createdAt: Date;
+
+  @CreateDateColumn()
+  createdAt?: Date;
+
+  @UpdateDateColumn()
+  updatedAt?: Date;
 }
